c24b589f00c3c422c2133c33dc1f1df28dcf2b1e
Raphaël Luta <raphael.luta@gmail.com>
2020-11-04 09:39:31 +0100

    use asText instead of render for quote content

--- components/post/card.js
+++ components/post/card.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {default as NextLink} from 'next/link'
-import Link from 'next/link'
 import {RichText} from 'prismic-reactjs'
-
-import {DocLink} from 'components'
 import {cardStyles} from 'styles'
 
 const Card = ({card}) => {
@@ -11,7 +7,7 @@ const Card = ({card}) => {
       <div className="card-container">
         <img className="photo" src={card.data.photo.url}/>
         <h2 className="title">{RichText.asText(card.data.title)}</h2>
-        <p className="quote">{RichText.render(card.data.quote)}</p>
+        <p className="quote">{RichText.asText(card.data.quote)}</p>
         <style jsx global>{cardStyles}</style>
       </div>
     );
